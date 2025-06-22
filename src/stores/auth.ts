7e153e58865926321e4485523a115521c4a2a8e3
Luan Victor <lvictor.dutra@gmail.com>
2025-06-22 10:41:35 -0300

    feat: add isInitialized state and setInitialized action to auth store

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -2,12 +2,16 @@ import { create } from "zustand";
 
 type AuthState = {
   accessToken: string | null;
+  isInitialized: boolean;
   setAccessToken: (token: string) => void;
   clearAccessToken: () => void;
+  setInitialized: () => void;
 };
 
 export const useAuthStore = create<AuthState>((set) => ({
   accessToken: null,
+  isInitialized: false,
   setAccessToken: (token) => set({ accessToken: token }),
   clearAccessToken: () => set({ accessToken: null }),
+  setInitialized: () => set({ isInitialized: true }),
 }));
