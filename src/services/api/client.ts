54a3982384c2ee1f91e08fc0db9c22faf0dee2d1
Luan Victor <lvictor.dutra@gmail.com>
2025-06-21 23:45:59 -0300

    feat: implement API client for handling private and public requests

---
+++ src/services/api/client.ts
@@ -0,0 +1,21 @@
+import { env } from '@/env';
+import { createApiClient } from '../../lib/createApiClient';
+
+const apiUrl =
+  env.VITE_ENV === 'development'
+    ? 'http://localhost:8080'
+    : 'not available in production';
+
+export const apiPrivateClient = createApiClient({
+  baseURL: apiUrl,
+  withCredentials: true,
+  includeAuthTokens: true,
+  headers: { 'Content-Type': 'application/json' },
+});
+
+export const apiPublicClient = createApiClient({
+  baseURL: apiUrl,
+  withCredentials: true,
+  includeAuthTokens: false,
+  headers: { 'Content-Type': 'application/json' },
+});
